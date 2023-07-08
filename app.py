55bf15702af0094918084edf608949f05f521d8a
msajid103 <msajidnaizi@gamil.com>
2023-07-08 06:18:30 -0700

    Only debug fals and host = 0000

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def index():
     return render_template("index.html",weight = [],profit = [], table=[])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
