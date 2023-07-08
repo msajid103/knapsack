8fea8737cae61564b481d1a882b60be940ce0c89
M Sajid <msajidnaizi@gamil.com>
2023-07-08 04:19:50 -0700

    add all files and the code is don aupto knapsack table

--- app.py
+++ app.py
@@ -1,37 +1,51 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-n = 12
-weights = [2, 4, 6, 3]
-items =   [5, 8, 14, 10]
-items = [x for _, x in sorted(zip(weights,items))]
-weights = sorted(weights)
-table = []
-# print(weights)
-for i in range(len(items)+1):
-    table.append([0]*(n+1))
 
-for i in range(1,len(items)+1):
-    for w in range(1,n+1):
-        if weights[i-1] <= w:
-            table[i][w] = max(table[i-1][w],(table[i-1][w-weights[i-1]])+items[i-1])
-        else:
-            table[i][w] = table[i-1][w]
+def knapsack(capacity,weight,profit):
 
-l = n
-ls = []
-for i in range(len(items),0,-1):
-    if table[i][l] == table[i-1][l]:
-        ls.append(0)
+    # sorting values according to weight
+    weight_value = sorted(weight)    
+    profit_value = []
+    for i in weight_value:
+        profit_value.append(profit[weight.index(i)])
 
-    else:
-        ls.append(weights[i-1])
-        l -= weights[i-1]
-print(ls)
+    # making table with first zero entries
+    table = []
+    for i in range(len(profit_value)+1):
+        table.append([0]*(capacity+1))   
 
+    # putting value into table
+    for row in range(1,len(profit_value)+1):
+        for colunm in range(1,capacity+1):
+            if weight_value[row-1] <= colunm:
+                table[row][colunm] = max(table[row-1][colunm],(table[row-1][colunm-weight_value[row-1]])+profit_value[row-1])
+            else:
+                table[row][colunm] = table[row-1][colunm]
 
-@app.route('/')
+    # back tracking
+    last_index = capacity
+    selected_values =sorted([])
+    for i in range(len(profit_value),0,-1):
+        if table[i][last_index] != table[i-1][last_index]:         
+            selected_values.append(weight_value[i-1])
+            last_index -= weight_value[i-1]
+
+    return weight_value,profit_value,table,selected_values     
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", ta = table)
+    if request.method == 'POST':
+        weight_capacity = int(request.form['weight_capacity'])
+        weight_values = list(map(int, request.form['weight_values'].split(',')))
+        profit_values = list(map(int, request.form['profit_values'].split(',')))
+
+        # calling knapsack function for calcultaion
+        weight_value,profit_value,table,selected_weights  =  knapsack(weight_capacity,weight_values,profit_values)       
+        
+        
+        return render_template("index.html",weight=[0]+weight_value,profit =[0]+ profit_value, table=table, selected_weights=selected_weights)
+    
+    return render_template("index.html",weight = [],profit = [], table=[])
 
 if __name__ == '__main__':
     app.run(debug=True)
