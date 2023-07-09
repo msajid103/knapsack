c8a8579e7fbf68c6e54f6bc9989242b1b44682a2
msajid103 <msajidnaizi@gamil.com>
2023-07-08 23:21:06 -0700

    generate requrements file and write some gunicorn in procfile for deployment

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def index():
     return render_template("index.html",weight = [],profit = [], table=[])
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
